dd8e982bb14a52bbe73de720eed6846e65269a42
whlucas <lll13999520192>
2020-08-24 10:21:44 +0800

    功能完事，没加入读txt

---
+++ go.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd /e/front_end/advanced/BaoLF_Steel/steel_information_inquiry
+
+npm start
---
+++ steel_information_inquiry/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MyTable from './table'
+
+function App() {
+  return (
+    <div className="App">
+      <MyTable></MyTable>
+    </div>
+  );
+}
+
+export default App;
